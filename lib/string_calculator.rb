418528a1bf771c812e965bc8c87968a54a570617
Kalpana Sharma <kalpana.sharma0795@gmail.com>
2025-05-20 16:50:02 +0530

    Remove unnecessary code

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,8 +2,6 @@ class StringCalculator
   def self.add(str)
     return 0 if str.empty?
 
-    return str.to_i if str.length == 1
-
     numbers = str.split(/,|\\n/).map(&:to_i)
     numbers.sum
   end
