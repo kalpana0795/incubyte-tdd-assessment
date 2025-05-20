4c1e1759d136a1a359196433db11c85ce08f571c
Kalpana Sharma <kalpana.sharma0795@gmail.com>
2025-05-20 17:01:51 +0530

    Handle custom delimeters

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,7 +2,14 @@ class StringCalculator
   def self.add(str)
     return 0 if str.empty?
 
-    numbers = str.split(/,|\\n/).map(&:to_i)
+    delimeter = /,|\\n/
+
+    if str.start_with?('//')
+      delimeter_str, str = str.split('\n')
+      delimeter = delimeter_str[2]
+    end
+
+    numbers = str.split(delimeter).map(&:to_i)
     numbers.sum
   end
 end
