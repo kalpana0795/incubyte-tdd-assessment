8f7d2ec3e8de52000952510543af2fbfac346391
Kalpana Sharma <kalpana.sharma0795@gmail.com>
2025-05-20 16:23:46 +0530

    Add test case for empty string

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,4 +1,11 @@
 require 'string_calculator'
 
 RSpec.describe StringCalculator do
+  describe '.add' do
+    context 'when string is empty' do
+      it 'returns 0' do
+        expect(described_class.add('')).to eq(0)
+      end
+    end
+  end
 end
