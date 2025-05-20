a840b8f4b1e530a577986d4bd1dcc38016eb8791
Kalpana Sharma <kalpana.sharma0795@gmail.com>
2025-05-20 16:30:46 +0530

    Refactor test cases

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,16 +1,22 @@
 require 'string_calculator'
 
 RSpec.describe StringCalculator do
+  subject(:string_sum) { described_class.add(str) }
+
   describe '.add' do
     context 'when string is empty' do
+      let(:str) { '' }
+
       it 'returns 0' do
-        expect(described_class.add('')).to eq(0)
+        expect(string_sum).to eq(0)
       end
     end
 
     context 'when string contains one number' do
+      let(:str) { '1' }
+
       it 'returns 1 for 1' do
-        expect(described_class.add('1')).to eq(1)
+        expect(string_sum).to eq(1)
       end
     end
   end
